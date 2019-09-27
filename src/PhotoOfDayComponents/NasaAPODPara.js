793ab4803c598e92f0fb031698e848d9a58547f0
Freddie <fthompson803@live.com>
2019-09-26 22:47:05 -0400

    adding reactstrap styles and styled components styles

---
+++ src/PhotoOfDayComponents/NasaAPODPara.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const myParagraph = styled.p`
+  font-family: 'Lucida Sans', Arial, sans-serif;
+  font-size: 16px;
+  line-height: 26px;
+  text-indent: 30px;
+  margin: 0 auto;
+  text-align: center;
+`;
+
+export default myParagraph;
